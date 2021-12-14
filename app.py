a952774998713582082f1a4e0c37f77916bc896e
Iulian Cozma <iulian.cozma@gmail.com>
2021-12-14 16:29:19 +0200

    test

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return "Hello World! 2"
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=8080)
