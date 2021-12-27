a1d8e8be9e09480bd5c94bf6fd02fc3241ebe590
icozma <iualian.cozma@gmail.com>
2021-12-27 23:39:14 +0200

    app local

---
+++ app_local.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from waitress import serve
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello World! 4"
+
+
+if __name__ == '__main__':
+    # serve(app, host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080)
