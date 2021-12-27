98f1fd931c425e28d2ac9f79bb5049704894bdf5
icozma <iualian.cozma@gmail.com>
2021-12-27 23:30:06 +0200

    fix rhel python image

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ from waitress import serve
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
-    return "Hello World! 2"
+    return "Hello World! 4"
+
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=8080)
+    # app.run(host='0.0.0.0', port=8080)
