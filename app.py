7dd8c0c7eb0882091302beacdea0777f5768f00c
silvium76 <marincas.silviu@gmail.com>
2022-04-06 16:13:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ def hello():
 def toys():
     return "Hello World from toys"
 
+@app.route('/cities')
+def cities():
+    return "Hello World from anywhere in the world"
+
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=8080)
     # app.run(host='0.0.0.0', port=8080)
