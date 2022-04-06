9283b26769b6353eb9876c6b735d591b314f8a08
silvium76 <marincas.silviu@gmail.com>
2022-04-06 15:57:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ app = Flask(__name__)
 def hello():
     return "Hello World! 4"
 
+@app.route('/toys')
+def toys():
+    return "Hello World from toys"
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=8080)
